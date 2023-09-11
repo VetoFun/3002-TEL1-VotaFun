4d6d011af7525e5872973092d2031df7437bdc7e
Lloyd Wang <LWANG046@e.ntu.edu.sg>
2023-09-11 13:40:32 +0800

    add .vscode folder and run pnpm lint

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       animation: {
-        "spin-slow": "spin 3s linear infinite",
+        'spin-slow': 'spin 3s linear infinite',
       },
     },
   },
   daisyui: {
-    themes: ["light", "dark", "cupcake", "dracula"],
+    themes: ['light', 'dark', 'cupcake', 'dracula'],
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 };
 export default config;
