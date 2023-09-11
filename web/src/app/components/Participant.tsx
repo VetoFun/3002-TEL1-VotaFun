fb4ebc934b0f5fe7e2b7a2ef42d759eea36c240a
Siddiq <devsfiq@gmail.com>
2023-09-11 11:01:30 +0800

    Add Prettier

--- web/src/app/components/Participant.tsx
+++ web/src/app/components/Participant.tsx
@@ -1,22 +1,21 @@
-
 interface ParticipantProps {
-    initial: string,
-    name: string
+  initial: string;
+  name: string;
 }
 
-function Participant({ initial, name } : ParticipantProps) {
-    return (
-    <div className='flex gap-4 px-4 py-2 align-middle w-full hover:bg-base-content hover:text-neutral-focus transition-colors'>
-        <div className="avatar placeholder">
-        <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
-            <span className="inline text-base">{initial}</span>
+function Participant({ initial, name }: ParticipantProps) {
+  return (
+    <div className="flex w-full gap-4 px-4 py-2 align-middle transition-colors hover:bg-base-content hover:text-neutral-focus">
+      <div className="avatar placeholder">
+        <div className="w-10 rounded-full bg-neutral-focus text-neutral-content">
+          <span className="inline text-base">{initial}</span>
         </div>
-        </div> 
-        <div className='flex-grow flex'>
-        <p className='my-auto'>{name}</p>
-        </div>
+      </div>
+      <div className="flex flex-grow">
+        <p className="my-auto">{name}</p>
+      </div>
     </div>
-    )
+  );
 }
 
-export { Participant }
+export { Participant };
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,33 +1,41 @@
-import { Participant } from "./components/Participant"
-import { VoteButton } from "./components/VoteButton"
+import { Participant } from "./components/Participant";
+import { VoteButton } from "./components/VoteButton";
 
 export default function Home() {
   return (
-    <div className='flex flex-col w-screen h-screen bg-secondary-content'>
+    <div className="flex h-screen w-screen flex-col bg-secondary-content">
       <nav className="navbar flex justify-between bg-base-200">
-        <a className="btn btn-ghost normal-case text-base" href="#menu">VotaFun</a>
+        <a className="btn btn-ghost text-base normal-case" href="#menu">
+          VotaFun
+        </a>
         <div>
-          <a className="btn btn-ghost normal-case text-base" href="create">Create Room</a>
-          <a className="btn btn-ghost normal-case text-base" href="join">Join Room</a>
+          <a className="btn btn-ghost text-base normal-case" href="create">
+            Create Room
+          </a>
+          <a className="btn btn-ghost text-base normal-case" href="join">
+            Join Room
+          </a>
         </div>
       </nav>
-      <div id='container' className='flex-grow flex'>
-        <main className='flex-grow py-6'>
-          <p className='text-center text-2xl mb-8'>Please select your preferred activity</p>
-          <div className='mx-auto w-1/3 grid grid-cols-2 grid-rows-2 gap-2'>
-            <VoteButton id='1' text='Option 1' />
-            <VoteButton id='2' text='Option 2' />
-            <VoteButton id='3' text='Option 3' />
-            <VoteButton id='4' text='Option 4' />
+      <div id="container" className="flex flex-grow">
+        <main className="flex-grow py-6">
+          <p className="mb-8 text-center text-2xl">
+            Please select your preferred activity
+          </p>
+          <div className="mx-auto grid w-1/3 grid-cols-2 grid-rows-2 gap-2">
+            <VoteButton id="1" text="Option 1" />
+            <VoteButton id="2" text="Option 2" />
+            <VoteButton id="3" text="Option 3" />
+            <VoteButton id="4" text="Option 4" />
           </div>
         </main>
-        <aside className='flex-none w-60 bg-base-100'>
-          <h2 className='text-xl mx-4 mt-4 mb-2 font-semibold'>Participants</h2>
-          <Participant initial={'A'} name={'Alice'}/>
-          <Participant initial={'B'} name={'Bob'}/>
-          <Participant initial={'C'} name={'Charlie'}/>
+        <aside className="w-60 flex-none bg-base-100">
+          <h2 className="mx-4 mb-2 mt-4 text-xl font-semibold">Participants</h2>
+          <Participant initial={"A"} name={"Alice"} />
+          <Participant initial={"B"} name={"Bob"} />
+          <Participant initial={"C"} name={"Charlie"} />
         </aside>
       </div>
     </div>
-  )
+  );
 }
