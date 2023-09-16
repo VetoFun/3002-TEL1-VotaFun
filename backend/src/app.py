5639de3982b6833aa35eaecfe023806ffe140ac4
Alphaeus Ng <78588510+AlphaeusNg@users.noreply.github.com>
2023-09-16 11:48:46 +0800

    Feature/redis connection (#20)
    * Redis setup; renamed to redisdb
    
    * added functions to query(), store(), delete(), get_user(), get_questions(), insert_questions()
    
    * refactored files from redis into backend. Updated 'redisdb' name to 'redis' in 'docker-compose.yml' and renamed 'redisdb' folder back to 'redis'
    
    * updated requirements.txt after pre-commit hook cleanup
    
    * decomposed database.py into different classes + refactored code
    
    * added pytest, added more functions for database.py, should be serviceable
    
    * renamed classes, refactored import statements, updated requirement.txt
    
    * modify database classes to use a more OOP approach
    
    * add pytest conftest and dummy test
    
    * add rommstatus enum
    
    * change readme
    
    * modify readme
    
    * moved logger
    
    * comment out test_database.py
    
    ---------
    
    Co-authored-by: roylau98 <roylau98@gmail.com>
    Co-authored-by: Lloyd Wang <LWANG046@e.ntu.edu.sg>
    Co-authored-by: Lloyd <59206079+Parzivalxx@users.noreply.github.com>

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,22 @@
+import os
+
 from flask import Flask
+from flask_cors import CORS
+
+from .database.Database import Database
 
 
 app = Flask(__name__)
+app_settings = os.environ.get("APP_SETTINGS", "src.config.DevelopmentConfig")
+app.config.from_object(app_settings)
+CORS(app, supports_credentials=True)
+
+redis_db = Database(
+    redis_url=app.config["REDIS_URL"],
+    redis_host=app.config["REDIS_HOST"],
+    redis_port=app.config["REDIS_PORT"],
+)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ backend/tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+import fakeredis
+
+
+@pytest.fixture
+def fake_redis():
+    # Create a fakeredis instance
+    fake = fakeredis.FakeStrictRedis(version=7)
+    yield fake
+    fake.flushall()
