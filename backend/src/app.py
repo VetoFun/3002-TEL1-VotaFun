87416c766c51da288cd86bb5cf4fab8f11259df6
CharlesTran267 <charlestran267@gmail.com>
2023-09-21 00:29:39 +0800

    minor

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,5 @@
 from . import create_app, socketio
 
-app = create_app()
-socketio.init_app(app)
-
 if __name__ == "__main__":
     app = create_app()
     socketio.run(app, port=5001, debug=True)  # use flask run --debug --port=<your port>
