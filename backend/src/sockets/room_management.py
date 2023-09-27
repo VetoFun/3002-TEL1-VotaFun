799618269f018017edf380146589c2db796ddfbb
Lloyd Wang <LWANG046@e.ntu.edu.sg>
2023-09-27 22:56:53 +0800

    revert room_managment.py

--- backend/src/sockets/room_management.py
+++ backend/src/sockets/room_management.py
@@ -1,24 +1,8 @@
-from flask import jsonify, request
-from flask_socketio import Namespace, emit, send, join_room, leave_room, close_room, rooms
+from flask import request
+from flask_socketio import Namespace, send, join_room, leave_room, close_room
 from flask import current_app as app
 
 from src.logger import logger
-from src.utils.Room import create_room_func
-
-
-def list_users(room_id):
-    """
-    List all users in a room.
-    :param room_id: room id
-    :return: list of users in the room
-    """
-    room_data = app.database.query_room_data(room_id=room_id)
-    return [
-        {
-            "user_id": user.user_id,
-            "user_name": user.user_name,
-            "is_host": room_data.host_id == user.user_id
-        } for user in room_data.users]
 
 
 class RoomManagement(Namespace):
@@ -55,20 +39,7 @@ class RoomManagement(Namespace):
 
         send("Socket disconnected successfully")
 
-    def on_create_room(self, data):
-        """
-        Route to handle post request to "/createroom".
-        :return: response code 200 if the operation succeed, otherwise response code 500 is returned.
-        """
-        database = app.database
-        try:
-            result = create_room_func(data, database)
-            emit('create_room', result)
-        except Exception:
-            return {"success": False, "error": "Internal Server Error"}, 500
-
     def on_join_room(self, data):
-        print(data)
         room_id = data["room_id"]
         user_name = data["user_name"]
 
@@ -81,32 +52,18 @@ class RoomManagement(Namespace):
             send(f"Room {room_id} is full")
             return
 
-        join_room(room_id, request.sid)
-        print(rooms())
+        join_room(room_id)
 
         # Add user to database
         app.database.add_user(room_id=room_id, user_id=request.sid, username=user_name)
-        room_data = app.database.query_room_data(room_id=room_id)
-
-        if room_data.number_of_user == 1:
-            # Set host
-            room_data.host_id = request.sid
-            app.database.store_room_data(room_id=room_id, room_data=room_data)
 
         # Send message to all users in room
-        emit('join_room', {
-            "success": True,
-            "room_id": room_id,
-            "user_id": request.sid,
-            "user_name": user_name,
-            "is_host": room_data.host_id == request.sid
-        }, to=room_id)
-        emit('update_room', list_users(room_id), to=room_id)
+        send(f"{user_name} has joined the room {room_id}", to=room_id)
 
     def on_leave_room(self, data):
         room_id = data["room_id"]
         user_name = data["user_name"]
-        leave_room(room_id, request.sid)
+        leave_room(room_id)
 
         # Remove user from database
         app.database.remove_user(room_id=room_id, user_id=request.sid)
@@ -117,7 +74,6 @@ class RoomManagement(Namespace):
             self.on_close_room(data)
 
         # Send message to all users in room
-        print(f"{user_name} has left the room {room_id}")
         send(f"{user_name} has left the room {room_id}", to=room_id)
 
     def on_close_room(self, data):
