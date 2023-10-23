345b75dd2cc89dddf019280b298d8a45c559700f
Siddiq <devsfiq@gmail.com>
2023-10-23 17:52:18 +0800

    Add Room Capacity config

---
+++ web/src/stores/useErrorStore.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+interface ErrorStore {
+    message: string;
+    getMessage: () => string;
+}
+
+const useErrorStore = create<ErrorStore>((set, get) => ({
+    message: '',
+    getMessage: () => {
+        const error = get().message;
+        set(() => ({ message: '' }));
+
+        return error;
+    }
+}));
+
+export { useErrorStore}
